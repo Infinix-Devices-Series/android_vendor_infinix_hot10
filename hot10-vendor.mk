7b273c16d4d6b3581456e5f49751ad94f7adabef
TheMalachite <eliasgheeraert@gmail.com>
2021-02-03 12:40:31 +0100

    hot10: Kang aptX blobs from begonia-user 10 QP1A.190711.020 20.11.18 release-keys
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- hot10-vendor.mk
+++ hot10-vendor.mk
@@ -16,3 +16,7 @@
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/infinix/hot10
+
+PRODUCT_COPY_FILES += \
+    vendor/infinix/hot10/proprietary/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptXHD_encoder.so \
+    vendor/infinix/hot10/proprietary/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptX_encoder.so
